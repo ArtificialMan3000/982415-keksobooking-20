445ccec754b30f98577b1a6211d3a2d8e72585c6
Aleksandr <legionerab3000@yandex.ru>
2020-06-20 01:09:08 +0300

    Создал модуль для работы с ajax

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
-// Деактивируем сайт
-window.page.deactivate();
+window.main = (function () {
+  // Деактивируем сайт
+  window.page.deactivate();
 
-// Активируем сайт
-// window.page.activate();
+  // Активируем сайт
+  // window.page.activate();
 
+  var onError = function (message) {
+    console.error(message);
+  };
+
+  var onSuccess = function (data) {
+    console.log(data);
+  };
+
+  window.ajax.load('https://javascript.pages.academy/keksobooking/unknown', onSuccess, onError);
+
+  window.ajax.load('https://javascript.pages.academy/keksobooking/data', onSuccess, onError);
+
+  window.ajax.load('https://api.github.com/user', onSuccess, onError);
+})();
