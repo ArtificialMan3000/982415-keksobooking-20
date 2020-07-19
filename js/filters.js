dcb5c99d052a6f2d6c20a70c1967e93aa8b540cf
Aleksandr <legionerab3000@yandex.ru>
2020-07-19 20:54:22 +0300

    Реализовал фильтры

--- js/filters.js
+++ js/filters.js
@@ -2,16 +2,31 @@
 
 // Модуль фильтров объявлений
 window.filters = (function () {
-  // Фильтры
+  // Форма фильтров
   var MAP_FILTERS = document.querySelector('.map__filters');
-  // Фильтр типа жилья
-  var TYPE_FILTER = MAP_FILTERS.querySelector('#housing-type');
-  // Фильтр цены
-  // var PRICE_FILTER = MAP_FILTERS.querySelector('#housing-price');
-  // Фильтр количества комнат
-  // var ROOMS_FILTER = MAP_FILTERS.querySelector('#housing-rooms');
-  // Фильтр количества гостей
-  // var GUESTS_FILTER = MAP_FILTERS.querySelector('#housing-guests');
+  // Фильтры
+  var Filters = {
+    TYPE: MAP_FILTERS.querySelector('#housing-type'),
+    PRICE: MAP_FILTERS.querySelector('#housing-price'),
+    ROOMS: MAP_FILTERS.querySelector('#housing-rooms'),
+    GUESTS: MAP_FILTERS.querySelector('#housing-guests'),
+    FEATURES: MAP_FILTERS.querySelectorAll('#housing-features input')
+  };
+  // Диапазоны цен
+  var PRICE_RANGES = {
+    'low': {
+      min: 0,
+      max: 10000
+    },
+    'middle': {
+      min: 10000,
+      max: 50000
+    },
+    'high': {
+      min: 50000,
+      max: Infinity
+    }
+  };
   // Количество показываемых объявлений
   var ADVERTS_LIMIT = 5;
 
@@ -40,13 +55,41 @@ window.filters = (function () {
 
   // Применяет фильтры к показываемым объявлениям
   var applyFilters = function (data) {
-    var type = TYPE_FILTER.value;
     var filteredData = data;
-    if (type !== 'any') {
-      filteredData = data.filter(function (elem) {
-        return elem.offer.type === type;
+    var type = Filters.TYPE.value;
+    var priceRange = Filters.PRICE.value;
+    var rooms = Filters.ROOMS.value;
+    var guests = Filters.GUESTS.value;
+    var features = Array.from(Filters.FEATURES)
+      .filter(function (featureElem) {
+        return featureElem.checked === true;
+      })
+      .map(function (featureElem) {
+        return featureElem.value;
       });
-    }
+    filteredData = data.filter(function (elem) {
+      if (type !== 'any' && type !== elem.offer.type) {
+        return false;
+      }
+      if (priceRange !== 'any' &&
+          (PRICE_RANGES[priceRange].min > elem.offer.price || PRICE_RANGES[priceRange].max < elem.offer.price)) {
+        return false;
+      }
+      if (rooms !== 'any' && Number(rooms) !== elem.offer.rooms) {
+        return false;
+      }
+      if (guests !== 'any' && Number(guests) !== elem.offer.guests) {
+        return false;
+      }
+      if (features.length > 0) {
+        for (var i = 0; i < features.length; i++) {
+          if (elem.offer.features.indexOf(features[i]) < 0) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
     filteredData = limitAdverts(filteredData);
     window.map.updatePinsOnMap(filteredData);
   };
@@ -58,12 +101,24 @@ window.filters = (function () {
 
   // Устанавливает обработчики на изменение фильтров
   var setFiltersHandlers = function () {
-    TYPE_FILTER.addEventListener('change', filterChangeHandler);
+    Filters.TYPE.addEventListener('change', filterChangeHandler);
+    Filters.PRICE.addEventListener('change', filterChangeHandler);
+    Filters.ROOMS.addEventListener('change', filterChangeHandler);
+    Filters.GUESTS.addEventListener('change', filterChangeHandler);
+    Filters.FEATURES.forEach(function (elem) {
+      elem.addEventListener('change', filterChangeHandler);
+    });
   };
 
   // Удаляет обработчики изменения фильтров
   var removeFiltersHandlers = function () {
-    TYPE_FILTER.removeEventListener('change', filterChangeHandler);
+    Filters.TYPE.removeEventListener('change', filterChangeHandler);
+    Filters.PRICE.removeEventListener('change', filterChangeHandler);
+    Filters.ROOMS.removeEventListener('change', filterChangeHandler);
+    Filters.GUESTS.removeEventListener('change', filterChangeHandler);
+    Filters.FEATURES.forEach(function (elem) {
+      elem.removeEventListener('change', filterChangeHandler);
+    });
   };
 
   return {
